1664f07aefa3cc5a13aa2d1e00d8dfa52415b44b
jctransfi <jctransfi@gmail.com>
2015-05-26 09:58:58 +0800

    Initial Commit
    Committing files based on existing PA codebase

---
+++ public/js/plugins.js
@@ -0,0 +1,68 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+function DropDown(el) {
+    this.dd = el;
+    this.placeholder = this.dd.children('span');
+    this.opts = this.dd.find('ul.dropdown > li');
+    this.val = '';
+    this.index = -1;
+    this.initEvents();
+}
+
+DropDown.prototype = {
+    initEvents : function() {
+        var obj = this;
+
+        obj.dd.on('click', function(event){
+            $(this).toggleClass('active');
+            return false;
+        });
+
+        obj.opts.on('click',function(){
+            var opt = $(this);
+            obj.val = opt.text();
+            obj.index = opt.index();
+            obj.placeholder.text('OP360 NMS: ' + obj.val);
+        });
+    },
+    getValue : function() {
+        return this.val;
+    },
+    getIndex : function() {
+        return this.index;
+    }
+}
+
+$(function() {
+
+    var dd = new DropDown( $('#dd') );
+
+    $(document).click(function() {
+        // all dropdowns
+        $('.wrapper-dropdown-1').removeClass('active');
+    });
+
+});
---
+++ server.js
@@ -0,0 +1,108 @@
+// var http = require('http');
+
+// //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+// var options = {
+//   host: 'dcoeng1-paev-1.tnc.virtela.cc',
+//   path: '/stats/search/findByVhidAndDescrAndEnddtBetween?descr=Serial0%2F0%2F0&endt=2015-01-01+12:00:00&stdt=2015-01-01+00:00:00&vhid=CABRCV1-CROCS-RTR-1',
+//   port: 8090,
+//   method: 'GET'
+
+// };
+
+// callback = function(response) {
+//   var str = '';
+
+//   //another chunk of data has been recieved, so append it to `str`
+//   response.on('data', function (chunk) {
+//     str += chunk;
+//   });
+
+//   //the whole response has been recieved, so we just print it out here
+//   response.on('end', function () {
+//     console.log(str);
+//   });
+// }
+
+// http.request(options, callback).end();
+
+var express = require('express');
+var app = express();
+
+// var port = process.env.PORT || 8090; // dev port
+var port = process.env.PORT || 80; // set our port
+
+// create our router
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+  // do logging
+  console.log('Something is happening.');
+  next();
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' }); 
+});
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears/:descr/:endt/:stdt/:vhid')
+
+  // get all the bears (accessed at GET http://localhost:8080/api/bears)
+  .get(function(req, res) {
+    var http = require('http');
+
+    var descr = req.params.descr;
+    var endt = req.params.endt;
+    var stdt = req.params.stdt;
+    var vhid = req.params.vhid;
+
+    var urlpath = '/stats/search/findByVhidAndDescrAndEnddtBetween?descr='+descr+'&endt='+endt+'&stdt='+stdt+'&vhid='+vhid;
+
+    //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+    var options = {
+      host: 'dcoeng1-paev-1.tnc.virtela.cc',
+      path: urlpath,
+      port: 8090,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+
+    };
+
+    callback = function(response) {
+      var str = '';
+
+      //another chunk of data has been recieved, so append it to `str`
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+
+      //the whole response has been recieved, so we just print it out here
+      response.on('end', function () {
+        var parsed = JSON.parse(str)
+        console.log(str);
+        res.json(parsed);
+      });
+    }
+
+    http.request(options, callback).end();
+  });
+
+app.use('/api', router);
+app.use(express.static('public'));
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+// var server = app.listen(3000, function () {
+
+//   var host = server.address().address;
+//   var port = server.address().port;
+
+//   console.log('Example app listening at http://%s:%s', host, port);
+
+// });
